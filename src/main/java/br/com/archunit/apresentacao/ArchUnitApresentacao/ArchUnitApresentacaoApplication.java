eb870724eccbf2f7681d46e0df0845c8e06aeab7
Lucas Pacheco Oliveira <esc_loliveira@uolinc.com>
2019-11-04 10:50:54 -0300

    Adicionando o projeto

---
+++ src/main/java/br/com/archunit/apresentacao/ArchUnitApresentacao/ArchUnitApresentacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.archunit.apresentacao.ArchUnitApresentacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchUnitApresentacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchUnitApresentacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/archunit/apresentacao/ArchUnitApresentacao/ArchUnitApresentacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.archunit.apresentacao.ArchUnitApresentacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchUnitApresentacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
